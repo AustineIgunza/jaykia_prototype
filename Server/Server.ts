6a12f56afe2207d89d5838be0dbcc817a7dca80f
Muchiri <llwmuchiri@gmail.com>
2026-05-25 10:10:45 +0300

    Real-time connection setup ongoing

--- Server/Server.ts
+++ Server/Server.ts
@@ -1,16 +1,31 @@
-import http, { IncomingMessage, ServerResponse } from "http";
+import http, { IncomingMessage, Server, ServerResponse } from "http";
 import { PORT } from "./Src/Config/Env.js";
-import { Info } from "./Utilities/Logger.js";
+import { ErrorMsg, Info, Warning } from "./Utilities/Logger.js";
 import Router from "./Router.js";
 import { Database } from "./Src/Config/DB.js";
+import { SocketIO } from "./Src/Modules/Socket/socket.service.js";
 
 const db = new Database();
 
-const httpServer = http.createServer(
-  (request: IncomingMessage, response: ServerResponse<IncomingMessage>) =>
-    Router(db, request, response),
-);
+const httpServer: Server = http.createServer(
+    (request: IncomingMessage, response: ServerResponse<IncomingMessage>) =>
+      Router(db, request, response),
+  ),
+  socketServer = new SocketIO(httpServer);
 
 httpServer.listen(PORT, () => {
-  Info(`Server connection established at port ${PORT}`);
+  try {
+    socketServer.establishConnection();
+    Info(`Server connection established at port ${PORT}, Socket is also live`);
+  } catch (error) {
+    ErrorMsg(error as Error);
+  }
+});
+
+process.on("uncaughtException", (error) => {
+  Warning("Uncaught exception");
+  ErrorMsg(error);
+});
+process.on("unhandledRejection", (reason) => {
+  Warning(`Uncaught promise rejection: ${reason}`);
 });
---
+++ Server/Src/Modules/Socket/socket.service.ts
@@ -0,0 +1,21 @@
+import type { Server } from "http";
+import { Server as SocketServer } from "socket.io";
+import type { createBookingDTO } from "../Bookings/booking.types.js";
+
+export class SocketIO {
+  public ioSocket: SocketServer;
+
+  constructor(public httpServer: Server) {
+    this.ioSocket = new SocketServer(httpServer, {
+      connectTimeout: 45000,
+    });
+
+    this.ioSocket.on("booking", (bookingData: createBookingDTO) => {});
+  }
+
+  establishConnection() {
+    this.ioSocket.listen(this.httpServer);
+  }
+
+  async sendMessage(message: any) {}
+}
